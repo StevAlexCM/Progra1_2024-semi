50cb7c7718f0580fd03ea3d24409c647a5bbfcf8
StevAlexCM <campossteven593@gmail.com>
2024-07-24 05:32:41 -0600

    consola de Promedio de las notas y computo

---
+++ LAB1Y2CALCULO/LAB1Y2CALCULO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1Y2CALCULO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab1: ");
+            double lab1 = parse
+
+        }
+    }
+}
