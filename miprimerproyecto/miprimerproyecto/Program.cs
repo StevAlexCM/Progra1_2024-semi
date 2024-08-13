6b7abc8d43822dfe183896f1049469406508de92
StevAlexCM <campossteven593@gmail.com>
2024-08-13 07:37:44 -0600

    creacion de una matriz

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -10,27 +10,12 @@ namespace miprimerproyecto
     {
         static void Main(string[] args)
         {
-            int[] serie = new int[] { 5, 4, 6, 8, 9 };
-            int suma = 0;
-            foreach (int num in serie)
+            //Uso de matrices
 
-                suma += num;
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
+            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
+            Console.WriteLine("El numero de la posicion 0 es {0}", numeros[3]);
+            Console.ReadKey();
 
-            if (edad >= 18)
-
-                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
-
-            if (edad < 18 )
-
-                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
-
-
-            decimal prom = suma / serie.Length;
-            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
-
-            Console.ReadLine();
 
         }
     }
