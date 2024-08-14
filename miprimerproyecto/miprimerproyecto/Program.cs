26ee7ec82dfbed0c6c9813b0428fbf08046abe27
StevAlexCM <campossteven593@gmail.com>
2024-08-14 09:57:49 -0600

    Desarrollo de ejercicio de la secuencia fibonacci

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -10,33 +10,28 @@ namespace miprimerproyecto
     {
         static void Main(string[] args)
         {
-            //Uso de matrices
-
-            double[,] matriz = new double[,] {
-                {0.01, 487.6, 0, 0 },
-                {487.61, 642.85, 10, 17.48},
-                {642.86, 915.81, 10, 32.70},
-                {915.82, 2058.67, 20,60.00},
-                {2058.68, 9999999, 30, 288.57},
-            };
-            Console.Write("sueldo");
-            double sueldo = double.Parse(Console.ReadLine());
-            double afp = sueldo * 6.25 / 100;
-            sueldo -= afp;
-
-            double isr = 0;
-            for (int i = 0; i<5; i++) {
-                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
-                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
+            int a = 0, b = 1, c = 1;
+            /*
+             a+b=c
+             0+1=1
+             1+0=1
+             1+1=2
+             2+1=3
+             3+2=5
+             5+3=8
+             8+5=13
+            ...
+             */
+            while (c < 100)
+            {
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+                b = a;
+                a = c;
+                c = a + b;
             }
-
-            sueldo -= isr;
-            Console.WriteLine("Sueldo: {0}, AFP: {1}, ISR: {2}", sueldo, afp, isr);
+            
 
             Console.ReadLine();
-
-
-
         }
     }
 }
