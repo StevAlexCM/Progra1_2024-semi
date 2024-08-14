a9efa5499eb9f94afcb414e3a592717ca812fb1c
StevAlexCM <campossteven593@gmail.com>
2024-08-13 21:44:57 -0600

    Sueldo e impuesto

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -12,9 +12,29 @@ namespace miprimerproyecto
         {
             //Uso de matrices
 
-            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            Console.WriteLine("El numero de la posicion 0 es {0}", numeros[3]);
-            Console.ReadKey();
+            double[,] matriz = new double[,] {
+                {0.01, 487.6, 0, 0 },
+                {487.61, 642.85, 10, 17.48},
+                {642.86, 915.81, 10, 32.70},
+                {915.82, 2058.67, 20,60.00},
+                {2058.68, 9999999, 30, 288.57},
+            };
+            Console.Write("sueldo");
+            double sueldo = double.Parse(Console.ReadLine());
+            double afp = sueldo * 6.25 / 100;
+            sueldo -= afp;
+
+            double isr = 0;
+            for (int i = 0; i<5; i++) {
+                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
+                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
+            }
+
+            sueldo -= isr;
+            Console.WriteLine("Sueldo: {0}, AFP: {1}, ISR: {2}", sueldo, afp, isr);
+
+            Console.ReadLine();
+
 
 
         }
