e183d23a2e617aaf0d19a0abeceaf8e92f50407f
StevAlexCM <campossteven593@gmail.com>
2024-08-20 07:37:06 -0600

    ejemplo de funciones

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -10,23 +10,15 @@ namespace miprimerproyecto
     {
         static void Main(string[] args)
         {
-          
-            int[] serie = new int[] { 5, 9, 4, 6, 3, 2, 7, 11, 15, 17, 21 };
-            foreach (int num in serie)
-            {
-                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "Par" : "Impar");
-                int i = 1,
-                    n = 0;
-                while (i <= num && n <= 2)
-                {
-                    if (num % i == 0)
-                    {
-                        n++;
-                    }
-                    i++; 
-                Console.WriteLine("EL numero {0} {1}", num, n <= 2 ? "es primo" : "NO es primo");
-            }
+
+            Console.WriteLine("La suma de 10+5={0}", suma());
+
             Console.ReadLine();
+        }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
         }
     }
 }
