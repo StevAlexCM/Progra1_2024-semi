fbab06fc336ce9db2fd7df968fd3d9599a5a9291
StevAlexCM <campossteven593@gmail.com>
2024-10-16 12:46:21 -0600

    Parcial 2

---
+++ Parcial_2/Parcial_2/Conexion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; 
+using System.Data.SqlClient;
+
+namespace Parcial_2
+{
+    internal class Conexion
+    {
+        SqlConnection miConexion = new SqlConnection();//Conectarme a la BD.
+        SqlCommand misComandos = new SqlCommand();//ejecutar SQL en la BD.
+        SqlDataAdapter miAdaptador = new SqlDataAdapter();//Intermediario entre mi BD y la aplicacion.
+        DataSet ds = new DataSet();//es la representacion grafica de la BD en memoria RAM.
+
+        public Conexion()
+        {
+            String cadenaConexion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Usuarios.mdf;Integrated Security=True;Connect Timeout=30";
+            miConexion.ConnectionString = cadenaConexion;
+            miConexion.Open();
+        }
+        public DataSet obtenerDatos()
+        {
+            ds.Clear();
+            misComandos.Connection = miConexion;
+
+            miAdaptador.SelectCommand = misComandos;
+            misComandos.CommandText = "SELECT * FROM usuarios";
+            miAdaptador.Fill(ds, "usuario");
+
+            return ds;
+        }
+        public String administrarUsuarios(String[] datos)
+        {
+            String sql = "";
+            if (datos[0] == "nuevo")
+            {
+                sql = "INSERT INTO usuarios(usuario, clave, nombre,  direccion, telefono) VALUES('" +
+                    datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "','" + datos[6] + "')";
+            }
+            else if (datos[0] == "modificar")
+            {
+                sql = "UPDATE alumnos SET usuario='" + datos[2] + "', clave='" + datos[3] + "', direccion='" + datos[4] + "', telefono='" +
+                    datos[5] + "' WHERE idUsuario=" + datos[1];
+            }
+            else if (datos[0] == "eliminar")
+            {
+                sql = "DELETE FROM alumnos WHERE idUsuario=" + datos[1];
+            }
+            return ejecutarSQL(sql);
+        }
+        private String ejecutarSQL(String sql)
+        {
+            try
+            {
+                misComandos.Connection = miConexion;
+                misComandos.CommandText = sql;
+                return misComandos.ExecuteNonQuery().ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+
+        }
+    }
+}
